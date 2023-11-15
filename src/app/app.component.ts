ef0db5112c9cc36bbe0983dadf1d06b02dede952
ManuelFragaFernandez <54152389@ibercaja.es>
2023-11-15 12:08:23 +0100

    implementación obtener version en appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  value = 'World';
+  public appVersion: string | number = 0;
+
+  constructor(private swUpdate: SwUpdate) {
+    //#region Check for updates
+    if (this.swUpdate.isEnabled) {
+      debugger;
+      console.log('Updates enabled');
+      this.swUpdate.activated.subscribe((upd) => {
+        console.log('Update activated');
+        window.location.reload();
+      });
+      this.swUpdate.available.subscribe(
+        (upd) => {
+          console.log('Update available');
+          if (confirm('Do you want to install the new version of the app?')) {
+            console.log('Activating update');
+            this.swUpdate.activateUpdate();
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      this.swUpdate
+        .checkForUpdate()
+        .then(() => {
+          console.log('Checking for updates');
+        })
+        .catch((error) => {
+          console.log('Could not check for app updates', error);
+        });
+    }
+    //#endregion
+  }
 }
