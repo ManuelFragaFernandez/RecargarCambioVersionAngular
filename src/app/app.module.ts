1b0a40cc9ab1cd9bc9df939c08d08504a2c981dd
ManuelFragaFernandez <54152389@ibercaja.es>
2023-11-15 13:11:33 +0100

    ServiceWorker funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { SwUpdate } from '@angular/service-worker';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  providers: [SwUpdate],
-  imports: [BrowserModule],
+  // providers: [SwUpdate],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
